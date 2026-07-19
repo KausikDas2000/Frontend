8d526430cad503f2fb571c893e5dbf317ee4079d
kausikDas <kd96668@gmail.com>
2026-07-19 22:23:01 +0530

    add url

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -30,7 +30,7 @@ const Contact = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/contact",
+        "flourishing-manatee-340b6b.netlify.app",
         formData
       );
 
